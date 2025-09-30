46d2f9b015690a9ae4f34d42d17a0dc04629748a
Jorge Garcia Ojeda <jor4300@gmail.com>
2025-09-30 11:46:13 -0300

    Se modifico la config de CORs

--- src/main/java/com/example/casagariitaapi/config/CorsConfig.java
+++ src/main/java/com/example/casagariitaapi/config/CorsConfig.java
@@ -9,19 +9,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Value("${FRONTEND_URL}")
     private String frontendUrl;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns(frontendUrl)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS")
+                        .allowedOriginPatterns(frontendUrl, "http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
     }
 }
+
